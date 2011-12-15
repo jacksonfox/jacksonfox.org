52b18f395280e923e8f610c1d20c21bdab10302c
Jackson Fox <jacksonfox@gmail.com>
2011-12-14 21:19:51 -0600

    Fixed remote path, added output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 REMOTE_SERVER=oenimal.net
-REMOTE_PATH=/home/jackson/sites/jacksonfox.org
+REMOTE_PATH=/home/sites/jacksonfox.org
 
+echo Deploying to $REMOTE_SERVER:$REMOTE_PATH
 rsync -rp --exclude '.git*' * $REMOTE_SERVER:$REMOTE_PATH
