55903a114ce582f4f897a7582d6adec1258b4bc6
Jackson Fox <jacksonfox@gmail.com>
2011-12-14 21:26:17 -0600

    Added exclude for .DS_Store files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ REMOTE_SERVER=oenimal.net
 REMOTE_PATH=/home/sites/jacksonfox.org
 
 echo Deploying to $REMOTE_SERVER:$REMOTE_PATH
-rsync -rp --exclude '.git*' * $REMOTE_SERVER:$REMOTE_PATH
+rsync -rvp --exclude='.git*' --exclude='.DS_Store' * $REMOTE_SERVER:$REMOTE_PATH
